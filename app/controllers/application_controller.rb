89485ffe3ec67acde63fddb9254fefd03bc663d2
gukki7 <duffy.may08@outlook.jp>
2022-09-05 16:44:44 +0000

    [Add] application controllerの編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    case resource
+    when Customer
+      customers_customers_my_page_path(resource)
+    when Admin
+      admins_customers_path
+    end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
 end
